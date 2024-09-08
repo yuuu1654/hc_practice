bc8e7969fd5ef8d1c6bc6bd045cd6ed438522436
森下勇樹 <>
2024-09-09 07:04:04 +0900

    create base template and logic

---
+++ JavaScript/js-todo/todo.js
@@ -0,0 +1,80 @@
+
+const onClickAdd = () => {
+  /**
+   * 追加ボタン押されたら、入力値を変数に移してフォーム内は空にする
+   * 必要な要素 (li, span, 編集button, 削除button)を作成
+   * 表示する文字、クラス名などを
+   * created-todoのidを持つdivに、上記要素をulにくっつける
+   * 削除ボタンが押されたら
+   */
+  const inputText = document.getElementById("input-text").value;
+  if (inputText === "") {
+    confirm("TODOを入力してください")
+    return
+  }
+  document.getElementById("input-text").value = ""; 
+  const li = document.createElement("li");
+  li.style = "list-style: none;"
+  li.className = "d-flex align-items-center bg-info py-2"
+
+  const checkbox = document.createElement("input")
+  checkbox.type = "checkbox"
+  checkbox.className = "form-check-input me-2"
+
+  const span = document.createElement("span");
+  span.innerText = inputText;
+  span.className = "form-control"
+  span.style.width = "250px"
+
+  const editButton = document.createElement("button")
+  editButton.className = "btn btn-primary ms-2 me-1"
+  editButton.innerText = "編集"
+  // 編集ボタンのクリックイベント
+  editButton.addEventListener("click", (e) => {
+    /**
+     * 編集ボタンをクリックしたら保存ボタンに切り替わる
+     *   spanタグを入力可能なinputに置換する
+     *   spanの中に書いてあった文字をinputに移す (inputは、innerTextではなくvalueを使う)
+     * 保存ボタン押したら編集ボタンに表示を切り替える
+     *   保存ボタン(editButton)の直前のinputを取得
+     *   新しいspanタグを作る
+     *   inputに書いた文字を新しいspanに移し替える
+     */
+    const currentElement = editButton.previousElementSibling
+    if (editButton.innerText === "編集") {
+      editButton.innerText = "保存"
+      const input = document.createElement("input")
+      input.type = "text"
+      input.value = currentElement.innerText
+      input.className = currentElement.className
+      input.style.width = currentElement.style.width
+      currentElement.replaceWith(input)
+      input.focus(); // 入力フィールドにフォーカスを当てる
+    } else {
+      editButton.innerText = "編集"
+      const span = document.createElement("span")
+      span.innerText = currentElement.value
+      span.className = currentElement.className
+      span.style.width = currentElement.style.width
+      currentElement.replaceWith(span)
+    }
+  })
+
+  const deleteButton = document.createElement("button")
+  deleteButton.className = "btn btn-danger"
+  deleteButton.innerText = "削除"
+  // 削除ボタンのクリックイベント
+  deleteButton.addEventListener("click", (e) => {
+    deleteButton.closest("li").remove();
+  })
+
+  li.appendChild(checkbox)
+  li.appendChild(span)
+  li.appendChild(editButton)
+  li.appendChild(deleteButton)
+
+  const ul = document.getElementById("created-todo");
+  ul.appendChild(li)
+}
+
+document.getElementById("add-todo").addEventListener("click", onClickAdd)
